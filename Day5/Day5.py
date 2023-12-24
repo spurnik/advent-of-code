64898ef12b33bf9c459c8376d7fcf538926251f8
spurnik <arnau6martinez@gmail.com>
2023-12-24 18:55:01 +0100

    Day5 completed

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -5,12 +5,14 @@
 
 ## Helper functions
 def parse_input():
-    seeds, maps = [], []
+    seeds, seeds_as_ranges, maps = [], [], []
     with open('./input', 'r') as file:
         for i, line in enumerate(file):
 
             # Retrieve first line for seeds
-            if i == 0: seeds = [ int(number) for number in line[6:-1].split() ]
+            if i == 0: 
+                seeds = [ int(number) for number in line[6:-1].split() ]
+                seeds_as_ranges = [ Range(seeds[i], seeds[i], seeds[i + 1]) for i in range(0, len(seeds), 2)]
 
             # Start new map
             elif 'map' in line: maps.append([])
@@ -23,7 +25,7 @@ def parse_input():
     maps = [Map(map_ranges) for map_ranges in maps]
 
     # Return the list of initial seeds and all the maps
-    return seeds, maps
+    return seeds, seeds_as_ranges, maps
 
 ## Entities
 class Map:
@@ -31,23 +33,58 @@ class Map:
         self.map_ranges = map_ranges
 
     def __getitem__(self, key):
-        if isinstance(key, Range): return self.__getitem_for_range(key)
+        if isinstance(key, list) and len(key) > 0 and isinstance(key[0], Range): 
+            return self.__getitem_for_ranges(key)
+        if isinstance(key, Range): return self.__getitem_for_ranges([key])
         for map_range in self.map_ranges:
             if key in map_range: return map_range[key]
         return key
 
-    def __getitem_for_range(self, key_range):
-        pass
+    def __getitem_for_ranges(self, initial_key_ranges):
+        non_mapped_key_ranges = initial_key_ranges
+        mapped_key_ranges = []
+
+        for map_range in self.map_ranges:
+            new_non_mapped_key_ranges = []
+            for key_range in non_mapped_key_ranges:
+                for result_range in map_range[ key_range ]:
+                    if result_range.mapped: mapped_key_ranges.append( result_range )
+                    else: new_non_mapped_key_ranges.append( result_range )
+
+            # try mapping the non-mapped key ranges with the rest of iterations
+            non_mapped_key_ranges = new_non_mapped_key_ranges
+
+        # return all the mapped ranges and all the non-mapped ranges (defaultly self-mapped)
+        return mapped_key_ranges + non_mapped_key_ranges
 
     def __repr__(self): return repr(self.map_ranges)
         
 class Range:
-    def __init__(self, dest, source, _range):
+
+    def __init__(self, dest, source, _range, mapped = False):
         self.range = _range
         self.source = (source, source + _range)
         self.dest = (dest, dest + _range)
+        self.mapped = mapped
+
+    def __eq__(self, __o: object):
+        return ( 
+            self.dest[0] == __o.dest[0] and 
+            self.source[0] == __o.source[0] and
+            self.range == __o.range 
+        )
 
     def __contains__(self, key):
+
+        # overlapping ranges
+        if isinstance(key, self.__class__):
+            return (
+                key.dest[0] in self or 
+                key.dest[1] - 1 in self or 
+                key.dest[0] <= self.source[0] and self.source[1] < key.dest[1]
+            )
+
+        # single element
         return self.source[0] <= key & key < self.source[1]
 
     def __getitem__(self, key):
@@ -55,13 +92,14 @@ class Range:
         # Map source -> dest ranges through this range map
         if isinstance(key, self.__class__): return self.__map_range(key) 
 
-        if self.__contains__(key):
+        if key in self:
             diff = key - self.source[0]
             return self.dest[0] + diff
-        raise KeyError(key)
+
+        raise KeyError(f"Key {key} is not in {self}")
 
     def __repr__(self):
-        return '(' + str(self.source[0]) + ', ' + str(self.dest[0]) + ', ' + str(self.range) + ')'
+        return '(' + str(self.source[0]) + ', ' + str(self.dest[0]) + ', ' + str(self.range) + ', ' + str(self.mapped) + ')'
 
     def __map_range(self, other_range):
         source0, source1 = other_range.source
@@ -69,49 +107,77 @@ class Range:
         _range = other_range.range
 
         # The whole range of destination values can be mappped using this range
-        if dest0 in self and dest1 in self:
-            return [ Range(self[dest0], source0, _range) ]
+        if dest0 in self and dest1 - 1 in self:
+            return [ Range(self[dest0], source0, _range, True) ]
 
         # The destination range mapping breaks in two, the first subset being mapped
         if dest0 in self:
             offset = self.source[1] - dest0
-            mapped_subrange = Range(self[dest0], source0, offset)
-            non_mapped_subrange = Range(dest0 + offset, source0 + offset, _range - offset)
+            mapped_subrange = Range(self[dest0], source0, offset, True)
+            non_mapped_subrange = Range(dest0 + offset, source0 + offset, _range - offset, False)
             return [ mapped_subrange, non_mapped_subrange ]
 
         # The destination range mapping breaks in two, the second subset being mapped
-        if dest1 in self:
+        if dest1 - 1 in self:
             offset = self.source[0] - dest0
-            non_mapped_subrange = Range(dest0, source0, offset)
-            mapped_subrange = Range(self[dest0 + offset], source0 + offset, _range - offset)
-            return [non_mapped_subrange, mapped_subrange]
+            non_mapped_subrange = Range(dest0, source0, offset, False)
+            mapped_subrange = Range(self[dest0 + offset], source0 + offset, _range - offset, True)
+            return [ non_mapped_subrange, mapped_subrange ]
 
-        # The ranges do not overlap, no mapping at all
-        return other_range
+        # The destination range mapping breaks in three, the middle subset being mapped
+        if dest0 <= self.source[0] and self.source[1] < dest1:
+            offset = self.source[0] - dest0
+            left_non_mapped_subrange = Range(dest0, source0, offset, False)
+            middle_mapped_subrange = Range(self[dest0 + offset], source0 + offset, self.range, True)
+            right_non_mapped_subrange = Range(dest0 + offset + self.range, source0 + offset + self.range, _range - offset - self.range, False)
+            return [ left_non_mapped_subrange, middle_mapped_subrange, right_non_mapped_subrange ]
 
+        # The ranges do not overlap, return the original range as non-mapped
+        return [ Range(dest0, source0, _range, False) ]
 
 ## Main Program
 if __name__ == "__main__":
 	
-    seeds, maps = parse_input()
+    seeds, seeds_as_ranges, maps = parse_input()
 
     print('seeds')
     print(seeds)
 
-    print('maps')
+    print('\nseeds as ranges')
+    print(seeds_as_ranges)
+
+    print('\nmaps')
     print(maps[0])
     print(maps[-1])
 
     seed_locations = []
     for seed in seeds:
         source = seed
-        print('\nTransforming seed:', seed)
+        print('\n\nTransforming seed:', seed)
         for map in maps:
             dest = map[source]
             print('\t', source, '==>', dest)
             source = dest
-        print("Seed Location:", dest)
+        print("\nSeed Location:", dest)
         seed_locations.append(dest)
 
-    print('Lowest Location Number corresponding to any of the initial seeds:')
+    print('\n\nLowest Location Number corresponding to any of the initial seeds:')
     print(min(seed_locations))
+
+    seed_location_ranges = []
+    for seed_range in seeds_as_ranges:
+        source_ranges = [ seed_range ]
+        print('\n\nTransforming seed range:', seed_range)
+        for map in maps:
+            dest_ranges = map[source_ranges]
+            print('\t', source_ranges, '==>', dest_ranges)
+            source_ranges = dest_ranges
+        print("\nSeed range locations:", dest_ranges)
+        seed_location_ranges += dest_ranges
+
+    sorted_seed_location_ranges = sorted(seed_location_ranges, key = lambda location: location.dest[0])
+    print("\n\nFinal seed location ranges:")
+    print(sorted_seed_location_ranges)
+
+    print('\n\nLowest Location Number corresponding to any of the initial seeds (but this time, as ranges):')
+    print(min(seed_location_ranges, key = lambda location: location.dest[0] ))
--- Day5/Day5_test.py
+++ Day5/Day5_test.py
@@ -28,12 +28,28 @@ class TestRange(unittest.TestCase):
             self.range_instance[50]
 
     def test_range_map_range(self):
-        other_range = Range(0, 48, 5)
-        mapped_ranges = self.range_instance[other_range]
-        self.assertIsInstance(mapped_ranges, list)
-        self.assertIsInstance(mapped_ranges[0], Range)
-        self.assertEqual(mappend_range.source)
-            
+		# There are four cases to test:
+        totally_overlapped_range = Range(99, 100, 1) # source is 100
+        self.assertTrue(totally_overlapped_range in self.range_instance)
+        mapped_totally_overlapped_range = self.range_instance[totally_overlapped_range]
+        self.assertIsInstance(mapped_totally_overlapped_range , list)
+        self.assertIsInstance(mapped_totally_overlapped_range[0], Range)
+        self.assertListEqual(mapped_totally_overlapped_range, [ Range(51, 100, 1) ])
+
+        initial_overlapping_range = Range(99, 99, 3) # seeds 99, 100, 101
+        self.assertTrue(initial_overlapping_range in self.range_instance)
+        mapped_initial_overlapping_range = self.range_instance[initial_overlapping_range ]
+        self.assertListEqual(mapped_initial_overlapping_range, [ Range(51, 99, 1), Range(100, 100, 2) ])
+
+        final_overlapping_range = Range(96, 30, 3) # source within 30, 31, 32
+        self.assertTrue(final_overlapping_range in self.range_instance)
+        mapped_final_overlapping_range = self.range_instance[final_overlapping_range]
+        self.assertListEqual(mapped_final_overlapping_range, [ Range(96, 30, 2), Range(50, 32, 1) ])
+
+        non_overlapping_range = Range(96, 96, 2) # seeds 96, 97
+        self.assertFalse(non_overlapping_range in self.range_instance)
+        mapped_non_overlapping_range = self.range_instance[non_overlapping_range]
+        self.assertListEqual(mapped_non_overlapping_range, [ non_overlapping_range ])
 
 class TestMap(unittest.TestCase):
     def setUp(self):
@@ -49,6 +65,25 @@ class TestMap(unittest.TestCase):
         self.assertEqual(self.map_instance[49], 49)
         self.assertEqual(self.map_instance[0], 0)
         self.assertEqual(self.map_instance[10000], 10000)
+
+    def test_getitem_for_range(self):
+        # again, four cases:
+        range_within_first_map_range = Range(99, 100, 1)
+        range_within_second_map_range = Range(60, 0, 38)
+        range_within_all_map_ranges = Range(60, 0, 39)
+        range_out_of_any_map_range = Range(101, 0, 10)
+
+        # resulting mappings
+        mapped_range_within_first_map_range = self.map_instance[range_within_first_map_range]
+        mapped_range_within_second_map_range = self.map_instance[range_within_second_map_range]
+        mapped_range_within_all_map_ranges = self.map_instance[range_within_all_map_ranges]
+        mapped_range_out_of_any_map_range = self.map_instance[range_out_of_any_map_range]
+
+        # assertions
+        self.assertListEqual(mapped_range_within_first_map_range, [ Range(51, 100, 1) ])
+        self.assertListEqual(mapped_range_within_second_map_range, [ Range(62, 0, 38) ])
+        self.assertListEqual(mapped_range_within_all_map_ranges, [ Range(50, 38, 1), Range(62, 0, 38) ])
+        self.assertListEqual(mapped_range_out_of_any_map_range, [ Range(101, 0, 10) ])
 
 if __name__ == '__main__':
     unittest.main()
